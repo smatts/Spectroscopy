357d88719a77f6eb865e729903afbc0a24869d0c
Markus Lippitz <markus.lippitz@uni-bayreuth.de>
2020-05-08 16:20:33 +0200

    NLO Kapitel aus Diss eingebaut

--- 0_AUX/1_fundamentals/5_aggregates/Absorption/abs_kroh.m
+++ 0_AUX/1_fundamentals/5_aggregates/Absorption/abs_kroh.m
@@ -20,7 +20,7 @@ data_c5 = d.data;
 conc = [0, 1e-5, 5e-6, 1e-6, 5e-7];
 %conc = conc .* 0 + 1;
 
-wl_fix = 790;
+wl_fix = 650;
 [~ , id_fix] = min(abs(data_c3(:,1) - wl_fix));
 [~ , id_fix_c2] = min(abs(data_c2(:,1) - wl_fix));
 
@@ -34,3 +34,39 @@ plot(data_c5(:,1),(data_c5(:,2) - data_c5(id_fix,2)) ./ conc(5))
 %plot(data_blank(:,1), (data_blank(:,2) - data_blank(id_fix,2)) )
 
 hold off
+
+%-------------
+wl1 = 650;
+[~ , id1a] = min(abs(data_c2(:,1) - wl1));
+wl2 = 570;
+[~ , id2a] = min(abs(data_c2(:,1) - wl2));
+
+a2 = data_c2(id1a:id2a,2) - data_c2(id_fix_c2, 2);
+a2 = a2 ./ max(a2);
+
+
+[~ , id1] = min(abs(data_c3(:,1) - wl1));
+[~ , id2] = min(abs(data_c3(:,1) - wl2));
+
+a3 = data_c3(id1:id2,2) - data_c3(id_fix,2);
+a4 = data_c4(id1:id2,2) - data_c4(id_fix,2);
+a5 = data_c5(id1:id2,2) - data_c5(id_fix,2);
+a3 = a3 ./ max(a3);
+a4 = a4 ./ max(a4);
+a5 = a5 ./ max(a5);
+
+plot(data_c2(id1a:id2a,1), a2)
+hold on
+
+plot(data_c3(id1:id2,1), a3)
+plot(data_c3(id1:id2,1), a4)
+plot(data_c3(id1:id2,1), a5)
+hold off
+
+
+
+
+
+
+
+
